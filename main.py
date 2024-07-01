4c8bf987c71183314ea0248eb0eb45b12ee221af
SUBITSHA M <107224608+SUBITSHA-M@users.noreply.github.com>
2024-07-01 09:00:33 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+import random
+game_images = [rock, paper, scissors]
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+com_choice = random.randint(0, 2)
+if user_choice >= 3 or user_choice < 0:
+    print("You typed an invalid number, you lose!")
+else:
+    print("You chose:")
+    print(game_images[user_choice])
+    print("Computer Choice:")
+    print(game_images[com_choice])
+    
+    if user_choice == 0 and com_choice == 2:
+        print("You win!")
+    elif user_choice == 2 and com_choice == 1:
+        print("You loose Computer win")
+    elif  com_choice > user_choice:
+        print("You loose Computer win")
+    elif  com_choice == user_choice:
+        print("Draw Match")
+    elif user_choice > com_choice:
+        print("You win!")
+
